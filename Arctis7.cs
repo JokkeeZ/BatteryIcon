6ad56aeaccc8dc6037e30846c2d5c0f91cbb6c71
Joel <11576776+JokkeeZ@users.noreply.github.com>
2024-12-05 18:02:34 +0200

    Refactoring and converted project to use .NET 9.0

--- Arctis7.cs
+++ Arctis7.cs
@@ -1,47 +1,40 @@
 ﻿using HidLibrary;
 
-namespace HeadsetBatteryIcon
+namespace HeadsetBatteryIcon;
+
+class Arctis7
 {
-	class Arctis7
+	private readonly HidDevice hidDevice;
+
+	public Arctis7()
 	{
-		private HidDevice? hidDevice;
+		hidDevice = InitializeDevice();
+	}
 
-		private Arctis7() { }
-
-		public static Arctis7? CreateDevice()
+	private static HidDevice InitializeDevice()
+	{
+		foreach (var device in HidDevices.Enumerate(4152, 0x12ad))
 		{
-			var device = new Arctis7
+			if (device.Capabilities.Usage > 1)
 			{
-				hidDevice = InitializeDevice()
-			};
-
-			return device.hidDevice != null ? device : null;
-		}
-
-		static HidDevice? InitializeDevice()
-		{
-			foreach (var device in HidDevices.Enumerate(4152, 0x12ad))
-			{
-				if (device.Capabilities.Usage > 1)
-				{
-					return device;
-				}
+				return device;
 			}
-
-			return null;
 		}
 
-		public int GetBatteryPercentage()
+		return null;
+	}
+
+	public int GetBatteryPercentage()
+	{
+		hidDevice?.Write([0x06, 0x18]);
+		var result = hidDevice?.Read();
+
+		if (result != null)
 		{
-			hidDevice?.Write(new byte[2] { 0x06, 0x18 });
-			var result = hidDevice?.Read();
-
-			if (result != null)
-			{
-				return result.Data[2] > 100 ? 100 : result.Data[2];
-			}	
-		
-			return 0;
+			// 2nd byte contains the battery charge.
+			return result.Data[2] > 100 ? 100 : result.Data[2];
 		}
+
+		return 0;
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,200 +1,92 @@
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Text;
 
-#nullable disable
+namespace HeadsetBatteryIcon;
 
-namespace HeadsetBatteryIcon
+static class Program
 {
-	static class Program
+	[STAThread]
+	static void Main()
 	{
-		[STAThread]
-		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new ArctisBatteryApplicationContext());
-		}
+		Application.EnableVisualStyles();
+		Application.SetCompatibleTextRenderingDefault(false);
+		Application.Run(new ArctisBatteryApplicationContext());
 	}
+}
 
-	class ArctisBatteryApplicationContext : ApplicationContext
+class ArctisBatteryApplicationContext : ApplicationContext
+{
+	private readonly NotifyIcon icon;
+	private readonly Arctis7 headset;
+
+	private readonly List<Icon> icons =
+	[
+		new("icons/testi_0.ico", new(128, 128)),
+		new("icons/testi_25.ico", new(128, 128)),
+		new("icons/testi_50.ico", new(128, 128)),
+		new("icons/testi_75.ico", new(128, 128)),
+		new("icons/testi_100.ico", new(128, 128))
+	];
+
+	public ArctisBatteryApplicationContext()
 	{
-		private readonly System.Threading.Timer timer;
-		private readonly NotifyIcon icon;
-		private readonly Arctis7 arctis;
+		headset = new();
 
-		private int latestBatteryPercentage = -1;
-		private DateTime latestTimestamp;
-
-		private readonly List<Icon> icons = new()
+		if (headset == null)
 		{
-			new("icons/testi_0.ico", new(128, 128)),
-			new("icons/testi_25.ico", new(128, 128)),
-			new("icons/testi_50.ico", new(128, 128)),
-			new("icons/testi_75.ico", new(128, 128)),
-			new("icons/testi_100.ico", new(128, 128))
-		};
-
-		private readonly List<BatteryDischargeTime> dischargeTimes = new();
-
-		public ArctisBatteryApplicationContext()
-		{
-			arctis = Arctis7.CreateDevice();
-			if (arctis == null)
-			{
-				File.AppendAllText("asd.txt", $"Device not found");
-				return;
-			}
-
-			latestTimestamp = DateTime.Now;
-
-			var contextMenuStrip = new ContextMenuStrip(new Container());
-			contextMenuStrip.Items.AddRange(new[]
-			{
-				new ToolStripMenuItem("Debug", null, OpenDebugPopup),
-				new ToolStripMenuItem("Exit", null, Exit)
-			});
-
-			icon = new NotifyIcon()
-			{
-				Icon = icons[0],
-				Visible = true,
-				ContextMenuStrip = contextMenuStrip
-			};
-
-			timer = new(CheckBatteryPercentage, null, 0, 1000 * 5);
+			MessageBox.Show("Error", "Arctis 7 couldn't be detected.");
+			return;
 		}
 
-		private void OpenDebugPopup(object sender, EventArgs e)
-		{
-			var sb = new StringBuilder();
-
-			var batteryPercentage = arctis.GetBatteryPercentage();
-			var estimatedDischargeTime = GetEstimatedDischargeTime();
-
-			if (dischargeTimes.Count > 0)
-			{
-				sb.AppendLine("Discharge times: ");
-				foreach (var time in dischargeTimes)
-				{
-					sb.AppendLine($"Timestamp: {time.Timestamp}, Interval: {time.Interval}");
-				}
-
-				sb.AppendLine("----------------------");
-				sb.AppendLine($"Latest discharge: {dischargeTimes.OrderByDescending(x => x.Timestamp).First().Timestamp}");
-				sb.AppendLine($"Avg time to lose 1%: {estimatedDischargeTime}");
-			}
-			sb.AppendLine();
-			sb.AppendLine($"Current percentage: {batteryPercentage}%");
-
-			if (dischargeTimes.Count > 0)
-			{
-				sb.AppendLine($"Estimated battery life left: {(estimatedDischargeTime == TimeSpan.Zero ? "Calculating" : estimatedDischargeTime * batteryPercentage)}");
-			}	
-
-
-			MessageBox.Show(sb.ToString(), "Debug stuff", MessageBoxButtons.OK, MessageBoxIcon.Information);
-		}
-
-		private void Exit(object sender, EventArgs e)
+		var contextMenuStrip = new ContextMenuStrip(new Container());
+		contextMenuStrip.Items.Add(new ToolStripMenuItem("Exit", null, (_, _) =>
 		{
 			icon.Visible = false;
 			Application.Exit();
-		}
+		}));
 
-		private void CheckBatteryPercentage(object state)
+		icon = new NotifyIcon()
 		{
-			var currentTimestamp = DateTime.Now;
-			var batteryPercentage = arctis.GetBatteryPercentage();
+			Icon = icons[0],
+			Visible = true,
+			ContextMenuStrip = contextMenuStrip
+		};
 
-			// Headset disconnected, skip battery check.
-			if (batteryPercentage == 0)
-			{
-				Debug.WriteLine($"Headset disconnected...");
-				SetIcon(batteryPercentage);
-				return;
-			}
-
-			// First check
-			if (latestBatteryPercentage == -1)
-			{
-				latestBatteryPercentage = batteryPercentage;
-				Debug.WriteLine($"First battery check done...");
-			}
-
-			// Battery has discharged 1%
-			if (batteryPercentage < latestBatteryPercentage)
-			{
-				dischargeTimes.Add(new()
-				{
-					Interval = currentTimestamp - latestTimestamp,
-					Timestamp = currentTimestamp,
-				});
-				
-				Debug.WriteLine($"From {latestBatteryPercentage}% to {batteryPercentage}% it took {currentTimestamp - latestTimestamp}");
-				File.AppendAllText("asd.txt", $"From {latestBatteryPercentage}% to {batteryPercentage}% it took {currentTimestamp - latestTimestamp}");
-				
-				// Set current time as latest one
-				latestTimestamp = currentTimestamp;
-				latestBatteryPercentage = batteryPercentage;
-			}
-
-			Debug.WriteLine($"5 seconds passed... {batteryPercentage}% left");
-			SetIcon(batteryPercentage);
-		}
-
-		private void SetIcon(int batteryPercentage)
-		{
-			icon.Icon = GetIconForPercentage(batteryPercentage);
-
-			if (batteryPercentage == 0)
-			{
-				icon.Text = "Headset disconnected";
-				return;
-			}
-
-			var estimatedDischargeTime = GetEstimatedDischargeTime() * batteryPercentage;
-
-			if (estimatedDischargeTime == TimeSpan.Zero)
-			{
-				icon.Text = $"Battery left: {batteryPercentage}%\r\n" +
-					$"Estimate left: Calculating...";
-				return;
-			}
-
-			icon.Text = $"Battery left: {batteryPercentage}%\r\n" +
-				$"Estimate left: {estimatedDischargeTime.Days}d {estimatedDischargeTime.Hours}h {estimatedDischargeTime.Minutes}m";
-		}
-
-		private TimeSpan GetEstimatedDischargeTime()
-		{
-			var latestPercentages = dischargeTimes.OrderByDescending(x => x.Timestamp).Take(5);
-
-			if (!latestPercentages.Any())
-			{
-				return TimeSpan.Zero;
-			}
-
-			var averageTime = new TimeSpan(Convert.ToInt64(latestPercentages.Average(x => x.Interval.Ticks)));
-			return averageTime;
-		}
-
-		private Icon GetIconForPercentage(int percentage)
-		{
-			return percentage switch
-			{
-				> 0 and <= 25 => icons[1],
-				> 25 and <= 50 => icons[2],
-				> 50 and <= 75 => icons[3],
-				> 75 and <= 100 => icons[4],
-				_ => icons[0]
-			};
-		}
+		_ = new System.Threading.Timer(CheckBatteryPercentage, null, 0, 5000);
 	}
 
-	class BatteryDischargeTime
+	private void CheckBatteryPercentage(object state)
 	{
-		public TimeSpan Interval { get; set; }
-		public DateTime Timestamp { get; set; }
+		var batteryPercentage = headset.GetBatteryPercentage();
+
+		// Headset disconnected, skip battery check.
+		if (batteryPercentage == 0)
+		{
+			Debug.WriteLine($"Headset disconnected...");
+			SetIcon(batteryPercentage);
+			return;
+		}
+
+		SetIcon(batteryPercentage);
+	}
+
+	private void SetIcon(int batteryPercentage)
+	{
+		icon.Icon = batteryPercentage switch
+		{
+			> 0 and <= 25 => icons[1],
+			> 25 and <= 50 => icons[2],
+			> 50 and <= 75 => icons[3],
+			> 75 and <= 100 => icons[4],
+			_ => icons[0]
+		};
+
+		if (batteryPercentage == 0)
+		{
+			icon.Text = "Headset disconnected";
+			return;
+		}
+
+		icon.Text = $"Headset battery remaining: {batteryPercentage}%";
 	}
 }
